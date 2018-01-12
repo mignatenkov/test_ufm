267e2e18e6ce994d7d04edb0c64960916e7c8f8b
DESKTOP-781S8T3\anduser <an.reut@andersenlab.com>
2018-01-12 16:56:23 +0300

    доработки по актору и шедулеру. так же доработка утилитарного класса обработки файлов

--- src/main/java/com/andersen/test_ufm/repository/FileDao.java
+++ src/main/java/com/andersen/test_ufm/repository/FileDao.java
@@ -1,15 +1,10 @@
-package com.andersen.test_ufm.dao.imp;
+package com.andersen.test_ufm.repository;
 
-import com.andersen.test_ufm.dao.FileDao;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,8 +12,8 @@ import java.util.List;
  * Класс реализующией методы доступа к данным
  */
 @Component
-public class FileDaoImp implements FileDao {
-    private static final Logger LOGGER = LoggerFactory.getLogger(FileDaoImp.class);
+public class FileDao {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileDao.class);
 
     public List<File> getListFile(){
         File folder = new File("c:/Users/anduser/Other/input");
